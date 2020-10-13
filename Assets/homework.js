01251b3586523842ce8003ddc5309b66b9bfa25e
MDC <markdcross@gmail.com>
2020-10-13 09:47:00 -0400

    initial commit and directory setup

---
+++ Assets/homework.js
@@ -0,0 +1,46 @@
+var homework = [
+    {
+        name: 'Weather Dashboard',
+        description:
+            'Using OpenWeather API to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS. Local Storage will store persistent data.',
+        tools: ['HTML', 'CSS', 'jQuery', 'AXAJ', 'Local Storage', 'Bootstrap'],
+        apis: ['OpenWeatherMap', 'Moment'],
+        img: './assets/img/weather-dashboard.png',
+        alt: 'Weather Dashboard',
+        repo: 'https://github.com/markdcross/weather-dashboard',
+        app: 'https://markdcross.github.io/weather-dashboard/',
+    },
+    {
+        name: 'Work Day Scheduler',
+        description:
+            'Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.',
+        tools: ['HTML', 'CSS', 'jQuery', 'Bootstrap', 'Local Storage'],
+        apis: [],
+        img: './assets/img/work-day-scheduler.png',
+        alt: 'Work Day Scheduler',
+        repo: 'https://github.com/markdcross/work-day-scheduler',
+        app: 'https://markdcross.github.io/work-day-scheduler/',
+    },
+    {
+        name: 'Code Quiz',
+        description:
+            'This short code quiz was created with HTML, CSS, and vanilla JS. The idea is simple - answer the questions before your time runs out. Every correct answer awards you 10 points, while every incorrect answer reduces your timer by 10 seconds. At the end of the quiz, you have the opportunity to save your score to a high scores list (via local storage). The questions cover JavaScript topics such as variables and arrays. The styling is simple but clean, and I recommend taking the quiz with your sound on.',
+        tools: ['HTML', 'CSS', 'JavaScript', 'Bootstrap'],
+        apis: [],
+        img: './assets/img/code-quiz.png',
+        alt: 'Code Quiz',
+        repo: 'https://github.com/markdcross/code-quiz',
+        app: 'https://markdcross.github.io/code-quiz/',
+    },
+    {
+        name: 'Password Generator',
+        description:
+            'The password is based on user-selected criteria, specifically length and character types to be included. The app runs in the browser and dynamically updates the HTML and CSS via JavaScript.',
+        tools: ['HTML', 'CSS', 'JavaScript'],
+        apis: [],
+        img: './assets/img/password-gen.png',
+        alt: 'Password Generator',
+        repo: 'https://github.com/markdcross/password-generator',
+        app: 'https://markdcross.github.io/password-generator',
+    },
+];
---
+++ Assets/projects.js
@@ -0,0 +1,34 @@
+var projects = [
+    {
+        name: 'Perspektiv',
+        description:
+            "Get to know the beautifully weird city of Richmond through its murals and food via a web application integrating multiple server-side APIs to display information about the city's art and food scenes.",
+        tools: [
+            'HTML',
+            'CSS',
+            'jQuery',
+            'Materialize CSS',
+            'Leaflet',
+            'OpenStreetMap',
+            'Mapbox',
+            'Map of Richmond Murals JSON',
+        ],
+        apis: ['Yelp', 'City of Richmond Open Data Portal'],
+        img: './assets/img/perspektiv.png',
+        alt: 'Perspektiv',
+        repo: 'https://github.com/markdcross/perspektiv',
+        app: 'https://markdcross.github.io/perspektiv/',
+        readme:
+            'https://github.com/markdcross/perspektiv/blob/master/README.md',
+    },
+    // {
+    //     name: "An Hour Out",
+    //     description: "My wife is a writer, so I'm putting together a travel blog for her. It covers all the things you can do within an hour of Richmond, VA. This is just a little pet project that I'm working on as I develop my skills (so expect a ton of commits as I learn new things and want to test them out in a non-tutorial environment).",
+    //     tools: ["html", "css", "jQuery", "Bootstrap"],
+    //     apis: [],
+    //     img:"./assets/img/an-hour-out.png",
+    //     alt:"An Hour Out",
+    //     repo:"https://github.com/markdcross/an-hour-out",
+    //     app:"https://markdcross.github.io/an-hour-out/",
+    // }
+];
